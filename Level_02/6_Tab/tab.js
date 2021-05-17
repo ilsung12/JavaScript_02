b5e6fb8f3dac1b3cefa5f89e121adb8e5f3979ad
ilsung12 <a01041118225@gmail.com>
2021-05-17 11:34:18 +0900

    ✅  6. Tab

---
+++ Level_02/6_Tab/tab.js
@@ -0,0 +1,37 @@
+// $('.tab-button')
+//   .eq(0)
+//   .click(function () {
+//     $('.tab-button').removeClass('active');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(0).addClass('active');
+//     $('.tab-content').eq(0).addClass('show');
+//   });
+
+// $('.tab-button')
+//   .eq(1)
+//   .click(function () {
+//     $('.tab-button').removeClass('active');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(1).addClass('active');
+//     $('.tab-content').eq(1).addClass('show');
+//   });
+
+// $('.tab-button')
+//   .eq(2)
+//   .click(function () {
+//     $('.tab-button').removeClass('active');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(2).addClass('active');
+//     $('.tab-content').eq(2).addClass('show');
+//   });
+
+for (let i = 0; i < $('.tab-button').length; i++) {
+  $('.tab-button')
+    .eq(i)
+    .click(function () {
+      $('.tab-button').removeClass('active');
+      $('.tab-content').removeClass('show');
+      $('.tab-button').eq(i).addClass('active');
+      $('.tab-content').eq(i).addClass('show');
+    });
+}
